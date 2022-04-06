a06c4bf5f5a48b90026529dae1aafd3acb3a3932
Daniel Beecham <daniel@lunix.se>
2022-04-06 19:47:29 +0200

    indenting

--- flake.nix
+++ flake.nix
@@ -1,35 +1,34 @@
 {
-    outputs = { self, nixpkgs, ...}: {
+  outputs = { self, nixpkgs, ...}: {
 
-      packages.x86_64-linux.default = nixpkgs.lib.makeOverridable self.derivations.default {
-        inherit (nixpkgs.legacyPackages.x86_64-linux.stdenv) mkDerivation;
-        inherit (nixpkgs.legacyPackages.x86_64-linux) ragel kconfig-frontends;
-      };
-
-      derivations.default = {
-        mkDerivation,
-        ragel,
-        kconfig-frontends,
-        nats ? { host = "127.0.0.1"; port = "4222"; }
-      }: 
-      mkDerivation {
-        name = "ljsyslog";
-        src = ./.;
-        depsBuildBuild = [ 
-          ragel 
-          kconfig-frontends 
-        ];
-        makeFlags = [ 
-          "Q="
-          "CONFIG_NATS_HOST=\"${nats.host}\""
-          "CONFIG_NATS_PORT=\"${nats.port}\""
-        ];
-        installPhase = ''
-          install -d --mode=0755 $out
-          install -d --mode=0755 $out/bin
-          install --mode=0755 ljsyslog.stripped $out/bin/ljsyslog
-        '';
-      };
-
+    packages.x86_64-linux.default = nixpkgs.lib.makeOverridable self.derivations.default {
+      inherit (nixpkgs.legacyPackages.x86_64-linux.stdenv) mkDerivation;
+      inherit (nixpkgs.legacyPackages.x86_64-linux) ragel kconfig-frontends;
     };
+
+    derivations.default = {
+      mkDerivation,
+      ragel,
+      kconfig-frontends,
+      nats ? { host = "127.0.0.1"; port = "4222"; }
+    }:
+    mkDerivation {
+      name = "ljsyslog";
+      src = ./.;
+      depsBuildBuild = [
+        ragel
+        kconfig-frontends
+      ];
+      makeFlags = [
+        "CONFIG_NATS_HOST=\"${nats.host}\""
+        "CONFIG_NATS_PORT=\"${nats.port}\""
+      ];
+      installPhase = ''
+        install -d --mode=0755 $out
+        install -d --mode=0755 $out/bin
+        install --mode=0755 ljsyslog.stripped $out/bin/ljsyslog
+      '';
+    };
+
+  };
 }
